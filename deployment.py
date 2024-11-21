9603bf4b7a502b2d4cbd5d5f91b117d0bb6403f9
Seman_Kim <sdoramkim@gmail.com>
2024-11-21 16:11:48 +0900

    함수에 주석 추가, deployment_input의 입력 변수에 저장될 list 포함

--- deployment.py
+++ deployment.py
@@ -12,6 +12,14 @@ from itertools import count
 
 
 def insert_image(card_image, location):
+    """
+    card_image: 이미지로 넣을 대상
+    
+    location: 이미지가 들어갈 위치
+    
+    엑셀 파일과 이름이 같은 폴더에 접근하여 이미지가 존재한다면 location을 참고하여 그 위치에 card_image의
+    이미지 파일을 엑셀에 삽입
+    """
     image_extensions = [".png", ".jpg", ".jpeg"]
     for ext in image_extensions:
         image_file_path = image_folder / f"{card_image}{ext}"
@@ -31,10 +39,13 @@ def insert_image(card_image, location):
     return False
 
 def name_and_way():
+    """
+    입력 받은 내용을 엑셀에 옮기기 쉽도록 가공하는 함수
+    """
     big_data = [[], []]
     while True:
         # 0번에 카드명, 1번에 방법
-        name_way_data = deployment_input("카드명과 방법을 입력하세요 (종료하려면 빈 입력): ", 0)
+        name_way_data = deployment_input("카드명과 방법을 입력하세요 (종료하려면 빈 입력): ", 0, my_deployment)
         if not name_way_data:
             try:
                 big_data[0].pop()
@@ -56,6 +67,13 @@ def name_and_way():
     return big_data
 
 def insert_deployment(ID_row_idx, ID_col_idx):
+    """
+    ID_row_idx: 기준 row 위치
+    
+    ID_col_idx: 기준 col 위치
+    
+    기준 위치에서부터 입력 받은 내용을 두줄로 엑셀로 옮기고 다음 입력이 기준 값(현재 15)을 넘으면 줄바꿈 처리
+    """
     start_ID_row_idx = ID_row_idx
     start_ID_col_idx = ID_col_idx
     big_data = name_and_way()
@@ -86,15 +104,15 @@ def insert_deployment(ID_row_idx, ID_col_idx):
 
 def find_location(current_target_idx=int, current_col_idx=int, targets=list, target_list=list, search_range=int):
     '''
-    current_target_idx = 앞선 column 중 가장 가까운 target 위치
+    current_target_idx: 앞선 column 중 가장 가까운 target 위치
     
-    current_col_idx = 현재 column의 위치
+    current_col_idx: 현재 column의 위치
     
-    targets = 찾을 내용
+    targets: 찾을 내용
     
-    target_list = target이 담겨있는 list의 내용
+    target_list: target이 담겨있는 list의 내용
     
-    search_range = target이 몇 column뒤 까지 있는지 찾을 범위
+    search_range: target이 몇 column뒤 까지 있는지 찾을 범위
     
     target_list에서 다음 targets 중 가장 가까운 target이 search_range 이내에 존재하는지 파악 후
     다음 target에서 column위치가 search_range를 넘으면 True, 아니면 False 반환
@@ -113,6 +131,9 @@ def find_location(current_target_idx=int, current_col_idx=int, targets=list, tar
 # 전개 텍스트 저장 함수
 def deployment_save(sheet, deployment):
     """
+    sheet: 텍스트가 저장될 sheet의 이름
+    
+    deployment: sheet에 저장될 텍스트
     """
     wb = load_workbook(deployment[0] + ".xlsx")
     ws = wb[sheet]
@@ -124,19 +145,25 @@ def deployment_save(sheet, deployment):
             wb.save(file_name)
             return print(f"{chr(64 + i)}번에 {sheet_name} 전개 텍스트 저장")
 
-def deployment_input(input_text, input_type):
+def deployment_input(input_text, input_type, save_list):
     """
-    input_type : 0이면 split(), 1이면 그대로 
+    input_text: input을 사용할 때 이용자에게 보여줄 메시지
+     
+    input_type: 0이면 split(), 1이면 그대로 
+    
+    save_list: input으로 입력받은 내용이 저장될 list
+    
+    deployment_save에서 사용하기 위해 텍스트를 save_list에 저장하고 반환하는 함수
     """
     user_input = input(input_text)
-    my_deployment.append(user_input)
+    save_list.append(user_input)
     if input_type:
         return user_input
     else:
         return user_input.split()
 
 my_deployment = list()
-file_name = deployment_input("수정할 파일명을 입력하세요 (확장자 제외): ", 1) + ".xlsx"
+file_name = deployment_input("수정할 파일명을 입력하세요 (확장자 제외): ", 1, my_deployment) + ".xlsx"
 image_folder = Path.cwd() / "이미지" / file_name.split(".xlsx")[0]
 alignment_style = Alignment(horizontal="center", vertical="center")
 font_style_card = Font(bold=True, size=30)
@@ -164,7 +191,7 @@ except FileNotFoundError:
     print(f"'{file_name}' 파일이 없어 새 파일을 생성합니다.")
 
 # 기존 시트 선택 또는 새로운 시트 생성
-sheet_name = deployment_input("작성할 시트명을 입력하세요: ", 1)
+sheet_name = deployment_input("작성할 시트명을 입력하세요: ", 1, my_deployment)
 if sheet_name in wb.sheetnames:
     # 기존 시트 삭제
     wb.remove(wb[sheet_name])
@@ -185,7 +212,7 @@ for col_idx, value in enumerate(sheet_name.split(), start=2):
 insert_deployment(3, 2)
 
 # 엔드 페이즈
-q = deployment_input("엔드 페이즈 행동을 만드시겠습니까? (넘어가려면 빈 입력): " , 1)
+q = deployment_input("엔드 페이즈 행동을 만드시겠습니까? (넘어가려면 빈 입력): " , 1, my_deployment)
 if q:
     for i in count(start=1):
         if ws.cell(row=ws.max_row, column=i).value is None:
@@ -223,39 +250,39 @@ for row in ws.iter_rows(min_row=result_row, max_row=result_row+1, min_col=8, max
         cell.border = border_style
 
 # 결과물 이미지 붙이기
-q = deployment_input("결과 필드를 만드시겠습니까? (넘어가려면 빈 입력): ", 1)
+q = deployment_input("결과 필드를 만드시겠습니까? (넘어가려면 빈 입력): ", 1, my_deployment)
 if q:
     result_data = [[], []]
     for i in range(2):
         if i == 0:
-            result = deployment_input("왼쪽 엑스트라 몬스터 존에 있는 카드를 입력해주세요 (넘어가려면 빈 입력): ", 1)
+            result = deployment_input("왼쪽 엑스트라 몬스터 존에 있는 카드를 입력해주세요 (넘어가려면 빈 입력): ", 1, my_deployment)
             if result:
                 result_data[0].append(result)
                 result_data[1].append([result_row, 4])
         else:
-            result = deployment_input("오른쪽 엑스트라 몬스터 존에 있는 카드를 입력해주세요 (넘어가려면 빈 입력): ", 1)
+            result = deployment_input("오른쪽 엑스트라 몬스터 존에 있는 카드를 입력해주세요 (넘어가려면 빈 입력): ", 1, my_deployment)
             if result:
                 result_data[0].append(result)
                 result_data[1].append([result_row, 6])
                 
     for i in range(1, 6):
-        result = deployment_input(f"{i}번 몬스터 존에 있는 카드를 입력해주세요 (넘어가려면 빈 입력): ", 1)
+        result = deployment_input(f"{i}번 몬스터 존에 있는 카드를 입력해주세요 (넘어가려면 빈 입력): ", 1, my_deployment)
         if result:
             result_data[0].append(result)
             result_data[1].append([result_row+1, 2+i])
             
     for i in range(1, 6):
-        result = deployment_input(f"{i}번 마법 & 함정 존에 있는 카드를 입력해주세요 (넘어가려면 빈 입력): ", 1)
+        result = deployment_input(f"{i}번 마법 & 함정 존에 있는 카드를 입력해주세요 (넘어가려면 빈 입력): ", 1, my_deployment)
         if result:
             result_data[0].append(result)
             result_data[1].append([result_row+2, 2+i])
     
-    result = deployment_input("필드 마법 존에 있는 카드를 입력해주세요 (넘어가려면 빈 입력): ", 1)
+    result = deployment_input("필드 마법 존에 있는 카드를 입력해주세요 (넘어가려면 빈 입력): ", 1, my_deployment)
     if result:
         result_data[0].append(result)
         result_data[1].append([result_row+1, 2])
         
-    result = deployment_input("엑스트라 덱에 있는 카드를 입력해주세요 (넘어가려면 빈 입력): ", 1)
+    result = deployment_input("엑스트라 덱에 있는 카드를 입력해주세요 (넘어가려면 빈 입력): ", 1, my_deployment)
     if result:
         result_data[0].append(result)
         result_data[1].append([result_row+2, 2])
@@ -264,7 +291,7 @@ if q:
         result_data[1].append([result_row+2, 2])
     
     for i in count():
-        result = deployment_input("제외 존에 보여주고 싶은 카드를 입력해주세요 (넘어가려면 빈 입력): ", 1)
+        result = deployment_input("제외 존에 보여주고 싶은 카드를 입력해주세요 (넘어가려면 빈 입력): ", 1, my_deployment)
         if not result:
             break
         else:
@@ -272,7 +299,7 @@ if q:
             result_data[1].append([result_row, 8+i])
         
     for i in count():
-        result = deployment_input("묘지 존에 보여주고 싶은 카드를 입력해주세요 (넘어가려면 빈 입력): ", 1)
+        result = deployment_input("묘지 존에 보여주고 싶은 카드를 입력해주세요 (넘어가려면 빈 입력): ", 1, my_deployment)
         if not result:
             break
         else:
@@ -284,12 +311,12 @@ if q:
     result_data[1].append([result_row+2, 8])
     
     # 패 상태 입력
-    result = deployment_input("남은 패 매수를 입력해주세요 (넘어가려면 빈 입력): ", 1)
+    result = deployment_input("남은 패 매수를 입력해주세요 (넘어가려면 빈 입력): ", 1, my_deployment)
     if result:
         hand_result = [[], []]
         hand_count = int(result)
         for i in range(1, hand_count+1):
-            result = deployment_input("패에 특정 카드가 존재한다면 카드명과 방법을 입력해주세요 (넘어가려면 빈 입력): ", 0)
+            result = deployment_input("패에 특정 카드가 존재한다면 카드명과 방법을 입력해주세요 (넘어가려면 빈 입력): ", 0, my_deployment)
             if not result:
                 for j in range(1, hand_count-(i-1)+1):
                     hand_result[0].append("패")
@@ -314,7 +341,7 @@ if q:
             ws.cell(row=row_idx, column=col_idx, value=data)
             
 
-q = deployment_input("상대 턴 움직임을 만드시겠습니까? (넘어가려면 빈 입력): ", 1)
+q = deployment_input("상대 턴 움직임을 만드시겠습니까? (넘어가려면 빈 입력): ", 1, my_deployment)
 if q:
     ws.cell(row=result_row+5, column=1, value="상대")
     insert_deployment(result_row+5, 2)
